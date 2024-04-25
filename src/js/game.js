cad451e2b0cfd36a4015fa0b9491216981aa8175
bpikaar <bobpikaar@gmail.com>
2024-04-25 15:28:02 +0200

    initial commit

---
+++ src/js/game.js
@@ -0,0 +1,70 @@
+import '../css/style.css';
+import {Actor, Engine, Font, Label, vec} from "excalibur";
+import {ResourceLoader, Resources} from './resources.js';
+
+export class Game extends Engine {
+
+    score = 0;
+    scoreLabel;
+
+    constructor() {
+        super({
+            width: 800,
+            height: 600,
+        });
+        this.start(ResourceLoader).then(() => this.startGame());
+    }
+
+    onInitialize(engine) {
+        console.log("initializing game");
+
+        this.scoreLabel = new Label({
+            text: 'Score: 0',
+            pos: vec(this.drawWidth / 2, 25),
+            font: new Font({ size: 30 }),
+        });
+        this.add(this.scoreLabel);
+    }
+
+    startGame() {
+        console.log("start de game!");
+
+        setInterval(() => {
+            this.spawnPile();
+        }, 500);
+    }
+
+    spawnPile() {
+        const isMole = Math.random() > 0.5;
+
+        const pile = new Actor();
+        this.add(pile);
+        pile.on('pointerdown', (evt) => this.handlePointerDown(pile, isMole));
+
+        switch (isMole) {
+            case true:
+                pile.graphics.use(Resources.Mole.toSprite());
+                break;
+            case false:
+                pile.graphics.use(Resources.DirtPile.toSprite());
+                break;
+        }
+
+        pile.pos.x = Math.random() * this.drawWidth;
+        pile.pos.y = Math.random() * this.drawHeight;
+
+    }
+
+    handlePointerDown(pile, isMole) {
+        console.log('pointer down!');
+        if (isMole) {
+            this.score++;
+        } else {
+            this.score--;
+        }
+        this.scoreLabel.text = `Score: ${this.score}`;
+        pile.kill();
+    }
+}
+
+new Game();
